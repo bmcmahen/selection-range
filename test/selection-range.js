1b4bad3f37aea4de1fdb8a4a46dfae99f4484096
Wes Todd <wes@wesleytodd.com>
2015-12-14 15:57:52 -0600

    Fixes #7 - limit text selection to the max length of the node textContent

--- test/selection-range.js
+++ test/selection-range.js
@@ -73,6 +73,14 @@ describe('selection-range', function(){
     assert(str == 'e is ');
   });
 
+  it('should not exceede the text length', function(){
+    var p = el.textContent.length + 1;
+    var pos = { start: p, end: p };
+    sRange(el, pos);
+    var r = sRange(el);
+    assert(r.start === el.textContent.length);
+    assert(r.end ===  el.textContent.length);
+  });
 
 });
 
